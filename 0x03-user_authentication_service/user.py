3dba9e37177807743e0186f6129534399ec4c44c
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-12-19 22:54:37 +0200

    x

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,10 +6,10 @@ Defines User model.
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-base = declarative_base()
+Base = declarative_base()
 
 
-class User(base):
+class User(Base):
     """
     User model.
     """
